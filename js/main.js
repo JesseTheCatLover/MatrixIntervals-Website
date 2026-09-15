9eca28197c55f1c2ea684cc0a6064185b996e675
Jesse <itsredx000@gmail.com>
2026-09-15 12:19:01 +0330

    First Implementation Of The Background

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 // Copyright 2026 JesseTheCatLover. All Rights Reserved.
 
+const notes = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B"
+];
+
+const matrix = document.getElementById("interval-matrix");
+
+for (const rowNote of notes) {
+    for (const columnNote of notes) {
+        const cell = document.createElement("div");
+
+        cell.className = "matrix-cell";
+
+        cell.innerHTML = `
+            <span class="matrix-note">${rowNote}</span>
+            <span class="matrix-divider">→</span>
+            <span class="matrix-note">${columnNote}</span>
+        `;
+
+        matrix.appendChild(cell);
+    }
+}
